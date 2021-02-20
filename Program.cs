75bebe2ab450ab136e1f9d1d98e677e1e5cd5b98
Alex <ssh1nyyooo@gmail.com>
2021-02-20 19:09:32 +0200

    project init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ 
+namespace HelloApp
+{ 
+    struct User
+    {
+        public int age;
+ 
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Age: {age}");
+        }
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User tom;
+            tom.age = 34;
+            tom.DisplayInfo();
+             
+            Console.ReadKey();
+        }
+    }
+}
